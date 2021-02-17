b93be5b04bb6e704abf6b9141b6cab28ca50de59
dipen <dipen.2052@gmail.com>
2021-02-17 17:27:16 +0545

    backend completed

--- api/models.py
+++ api/models.py
@@ -7,9 +7,27 @@ class Movie(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=250)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        rating_sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            rating_sum += rating.stars
+        if len(ratings) > 0:
+            return rating_sum / len(ratings)
+        else:
+            return 0
+
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,11 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import  include
+from .views import MovieViewSet, RatingViewSet, UserViewSet
 router = routers.DefaultRouter()
+router.register('movie', MovieViewSet)
+router.register('ratings', RatingViewSet)
+router.register('users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 
